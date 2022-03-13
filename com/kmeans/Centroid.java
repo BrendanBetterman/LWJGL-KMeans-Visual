0fa5431674fee087c1fa6fddc0d058e9171e1288
BrendanBetterman <58009876+BrendanBetterman@users.noreply.github.com>
2022-03-13 18:51:00 -0500

    made multidimentional

--- com/kmeans/Centroid.java
+++ com/kmeans/Centroid.java
@@ -1,5 +1,38 @@
 package com.kmeans;
 
 public class Centroid {
+    private float[] vectors;
+    private float[] avgVec;
+
+    private int size;
+    public Centroid(float[] Vec){
+        set(Vec);
+    }
     
+    public void set(float[] Vec){
+        this.vectors = Vec;
+        this.size =0;
+        this.avgVec = new float[Vec.length];
+        for(int i =0; i < Vec.length; i++){
+            this.avgVec[i] = 0;
+        } 
+    }
+    public void addAvg(float[] Vec){
+        for(int i =0; i<Vec.length; i++){
+            this.avgVec[i] = Vec[i];
+        }
+        this.size++;
+    }
+    public boolean move(){
+        if(this.size == 0){
+            //move random
+            return false;
+        }
+        float[] tmp = new float[this.avgVec.length];
+        for(int i=0; i < this.avgVec.length; i++){
+            tmp[i] = this.avgVec[i]/this.size;
+        }
+        set(tmp);
+        return true;
+    }
 }
--- com/kmeans/Datapoint.java
+++ com/kmeans/Datapoint.java
@@ -1,10 +1,29 @@
 package com.kmeans;
-
+import com.kmeans.Centroid;
 public class Datapoint {
     private float x;
     private float y;
+    private Centroid id;
     public Datapoint(float x,float y){
         this.x = x;
+        this.y = y;
+    }
+    public void setId(Centroid id) {
+        this.id = id;
+    }
+    public Centroid getId() {
+        return id;
+    }
+    public float getX() {
+        return x;
+    }
+    public float getY() {
+        return y;
+    }
+    public void setX(float x) {
+        this.x = x;
+    }
+    public void setY(float y) {
         this.y = y;
     }
 }
